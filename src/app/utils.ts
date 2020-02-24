8758df4e3bfaa8bbddafd0ca02a87a4c4fbc55bb
Matthias Klumpp <matthias@tenstral.net>
2020-02-24 01:38:26 +0100

    Validate form input for correctness
    There are still ways to cheat, but I assume users want to produce
    actually valid metadata and will behave... ;-)

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -6,7 +6,10 @@ export function guessComponentId(homepage: string, appName: string): string
 
     let url;
     try {
-        url = new URL(homepage);
+        let urlStr = homepage;
+        if (!urlStr.startsWith('http'))
+            urlStr = 'https://' + homepage;
+        url = new URL(urlStr);
     } catch { return ''; }
 
     let rDNSRootParts = url.host.split('.').reverse()
@@ -33,4 +36,61 @@ export function guessComponentId(homepage: string, appName: string): string
              .replace(/"/g, '')
              .replace(/'/g, '');
     return tmp;
+}
+
+export interface CIDValidationResult {
+  valid: boolean;
+  message: string;
+}
+
+export function componentIdValid(cid: string): CIDValidationResult
+{
+    let res: CIDValidationResult = {valid: false, message: 'ID was empty'}
+    if (!cid)
+        return res;
+
+    let rDNSParts = cid.split('.')
+    if (rDNSParts.length < 3) {
+        res.message = 'ID does not follow the reverse-DNS scheme';
+        return res;
+    }
+
+    for (let i = 0; i < rDNSParts.length; i++) {
+        if (!rDNSParts[i].trim()) {
+            res.message = 'ID contains an empty segment.';
+            return res;
+        }
+    }
+
+    let ascii = /^[ -~]+$/;
+    if (!ascii.test(cid)) {
+        res.message = 'ID contains non-ASCII characters.';
+        return res;
+    }
+
+    if (cid.includes(' ')) {
+        res.message = 'ID contains spaces.';
+        return res;
+    }
+
+    if (cid.includes('-')) {
+        res.message = 'ID contains hyphens.';
+        return res;
+    }
+
+    for (let i = 0; i < cid.length; i++) {
+        let code = cid.charCodeAt(i);
+        if (!(code > 47 && code < 58) && // numeric (0-9)
+            !(code > 64 && code < 91) && // upper alpha (A-Z)
+            !(code > 96 && code < 123)) { // lower alpha (a-z)
+                if ((cid[i] != '_') && (cid[i] != '.')) {
+                    res.message = `ID contains invalid character: ${cid[i]}`;
+                    return res;
+                }
+        }
+    }
+
+    res.message = null;
+    res.valid = true;
+    return res;
 }
